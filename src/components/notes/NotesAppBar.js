680104a00242e1ebb521484b9aa3b5c9d9fab154
alejandropl07 <aprietoleon97@gmail.com>
2022-07-09 14:43:23 -0500

    NoteAppBar component

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotesAppBar = () => {
+  return (
+    <div className="notes__app-bar">
+      <span>25 de mayo 2022</span>
+      <div>
+        <button className="btn">Picture</button>
+        <button className="btn">Save</button>
+      </div>
+    </div>
+  );
+};
+
+export default NotesAppBar;

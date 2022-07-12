5c5ca646760a42cbdb57e9fd67fc6095bfff6ac8
alejandropl07 <aprietoleon97@gmail.com>
2022-07-12 00:00:44 -0500

    Config redux toolkit

---
+++ src/features/auth/authSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const authSlice = createSlice({
+  name: "auth",
+  initialState: {},
+  reducers: {
+    login: (state, action) => {
+      return {
+        uid: action.payload.uid,
+        name: action.payload.displayName,
+      };
+    },
+    logout: (state) => {
+      return {};
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function
+export const { login, logout } = authSlice.actions;
+
+export default authSlice.reducer;

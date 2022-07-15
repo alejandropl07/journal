939172f417931ee0ea9551a5950015bdbfc374a3
alejandropl07 <aprietoleon97@gmail.com>
2022-07-14 23:39:54 -0500

    form Register

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,34 +1,57 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useForm } from "../../hooks/useForm";
 
 const RegisterScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    name: "Alejandro",
+    email: "aprietoleon97@gmail.com",
+    password: "1111",
+    password2: "1111",
+  });
+
+  const { name, email, password, password2 } = formValues;
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(name, email, password);
+  };
+
   return (
     <>
       <h3 className="auth__title">Register</h3>
-      <form>
+      <form onSubmit={handleRegister}>
         <input
           className="auth__input"
           type="text"
           name="name"
           placeholder="Name"
+          value={name}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
           type="email"
           name="email"
           placeholder="Email"
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
           type="password"
           name="password"
           placeholder="Password"
+          value={password}
+          onChange={handleInputChange}
         />
         <input
           className="auth__input"
           type="password"
           name="password2"
           placeholder="Confirm password"
+          value={password2}
+          onChange={handleInputChange}
         />
         <button type="submit" className="btn btn-primary btn-block  mb-5">
           Register
